59ed38918c17828331e3e9010777cd2f0a104c47
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-19 19:50:35 +0100

    fix: fragment changed to div so styling can be add

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,11 @@ import Navbar from './Navbar'
 
 const Layout = ({children}) => {
     return ( 
-        <>
+        <div className='content'>
             <Navbar />
             {children}
             <Footer/>
-        </>
+        </div>
 
      );
 }
