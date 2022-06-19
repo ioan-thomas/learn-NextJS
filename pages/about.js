e8a971eb44138e7068a7ec7d34e8b0f86888a2c0
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-19 16:31:11 +0100

    testing routing in next

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel officia est inventore repellendus sequi exercitationem molestiae modi placeat, sit amet consectetur error doloremque atque animi alias aperiam deserunt dolorem odio.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel officia est inventore repellendus sequi exercitationem molestiae modi placeat, sit amet consectetur error doloremque atque animi alias aperiam deserunt dolorem odio.</p>
+        </div>
+     );
+}
+ 
+export default About;
