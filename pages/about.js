314419a6337ca58bd869a1bda20924bcb769e9bf
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-20 11:21:03 +0100

    feat: header component to add metadata

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,17 @@
+import Head from 'next/head'
+
 const About = () => {
     return ( 
-        <div>
-            <h1>About</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel officia est inventore repellendus sequi exercitationem molestiae modi placeat, sit amet consectetur error doloremque atque animi alias aperiam deserunt dolorem odio.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel officia est inventore repellendus sequi exercitationem molestiae modi placeat, sit amet consectetur error doloremque atque animi alias aperiam deserunt dolorem odio.</p>
-        </div>
+        <>
+            <Head>
+                <title>Ninjas List| About</title>
+            </Head>
+            <div>
+                <h1>About</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel officia est inventore repellendus sequi exercitationem molestiae modi placeat, sit amet consectetur error doloremque atque animi alias aperiam deserunt dolorem odio.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel officia est inventore repellendus sequi exercitationem molestiae modi placeat, sit amet consectetur error doloremque atque animi alias aperiam deserunt dolorem odio.</p>
+            </div>
+        </>
      );
 }
  
