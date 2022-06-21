c7ece495f22b19a13085bef7057793dd5d47929e
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-21 13:37:40 +0100

    feat: created path for each route/ninja on build

---
+++ pages/ninjas/[id].js
@@ -0,0 +1,25 @@
+export const getStaticPages = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json()
+
+    const paths = data.map(ninja => {
+
+        return {
+            params: { id: ninja.id.toString() }
+        }
+    })
+
+    return {
+        paths, 
+        fallback: false
+    }
+}
+ 
+
+const Details = () => {
+    return ( 
+        <h1>Details Page</h1>
+     );
+}
+ 
+export default Details;
